134a18b9ee3feffc1ce050ba538d040193bcb1dd
saksh <saksh@host.docker.internal>
2024-01-05 18:59:49 +0530

    Initial Commit

---
+++ sakshi/src/sakshi/git.java
@@ -0,0 +1,16 @@
+package sakshi;
+
+import java.util.Scanner;
+
+public class git {
+
+		public static void main(String[] args) {
+			Scanner sc=new Scanner(System.in);
+			System.out.print("Enter the value of a:");
+			int a=sc.nextInt();
+			System.out.print("Enter the value of b:");
+			int b=sc.nextInt();
+			System.out.print("The output is found to be:"+(a+b));
+	}
+
+}
