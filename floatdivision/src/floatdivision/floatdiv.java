66866c0f7e464feb79c9167a8f5ea110e2dbd127
saksh <saksh@SAKSHIBHARATH>
2024-01-06 18:21:09 +0530

    final commit

---
+++ floatdivision/src/floatdivision/floatdiv.java
@@ -0,0 +1,16 @@
+package floatdivision;
+import java.util.*;
+public class floatdiv {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a,b;
+		System.out.println("Enter the first value:");
+		a=sc.nextInt();
+		b=sc.nextInt();
+		float c=(float)a/b;
+		System.out.println(c);
+		
+	}
+
+}
